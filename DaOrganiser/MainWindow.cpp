0b30f6215b66737ba2db2f9c4b6ad23a82b34798
Kenneth Ang <ksami.ihide@gmail.com>
2013-09-23 12:19:30 +0800

    initial commit

---
+++ DaOrganiser/MainWindow.cpp
@@ -0,0 +1,69 @@
+#include "stdafx.h"
+#include <msclr\marshal_cppstd.h>
+
+using namespace DaOrganiser;
+using namespace msclr::interop;
+
+/*void MainWindow::addTaskToList(Task task)
+{
+	// ASSUMPTION: user does not add tasks within 1 second of another
+	// use datetime stamp as unique identifier for each task
+	System::DateTime timeAdded=System::DateTime::Now;
+	ListViewItem^ taskToAdd=gcnew ListViewItem(timeAdded.ToString());
+	item->SubItems->Add(task.detail.ToString());
+}*/
+
+std::string MainWindow::getUserInput(void)
+{
+	return marshal_as<std::string> (textBox1->Text);
+}
+
+void MainWindow::clearInputField(void)
+{
+	textBox1->Text="";
+}
+
+void MainWindow::appendToOutput(std::string userFeedback)
+{
+	//check if richTextBox1 overflow
+	//if so, delete first 1000 characters
+	if(richTextBox1->Text->Length > 100000)
+	{
+		richTextBox1->Text = richTextBox1->Text->Remove(0, 1000);
+	}
+
+	richTextBox1->Text+=marshal_as<String^> (userFeedback);
+	richTextBox1->Select(richTextBox1->Text->Length - 1, 0);
+	richTextBox1->ScrollToCaret();
+	richTextBox1->Text+="\n";
+}
+
+void MainWindow::addToList(String^ details)
+{
+	System::DateTime timeAdded = System::DateTime::Now;
+	ListViewItem^ item = gcnew ListViewItem(timeAdded.ToString());
+	item->SubItems->Add(details);
+	item->SubItems->Add("Not done");
+	listView1->Items->Add(item);
+	delete item;
+}
+
+System::Void MainWindow::textBox1_KeyDown(System::Object^ sender, System::Windows::Forms::KeyEventArgs^ e)
+{
+	if(e->KeyCode == System::Windows::Forms::Keys::Enter)
+	{
+		richTextBox1->Text += "\n";
+		richTextBox1->Text += textBox1->Text;
+		addToList(textBox1->Text);
+		textBox1->Text = "";
+
+		richTextBox1->Select(richTextBox1->Text->Length - 1, 0);
+		richTextBox1->ScrollToCaret();
+
+		//check if richTextBox1 overflow
+		if(richTextBox1->Text->Length > 100000)
+		{
+			richTextBox1->Text = richTextBox1->Text->Remove(0, 1000);
+		}
+	}
+}
---
+++ DaOrganiser/MainWindow.h
@@ -0,0 +1,177 @@
+#pragma once
+
+#include "stdafx.h"
+#include <string>
+
+namespace DaOrganiser {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+	using namespace std;
+
+	/// <summary>
+	/// Summary for MainWindow
+	/// </summary>
+	public ref class MainWindow : public System::Windows::Forms::Form
+	{
+	public:
+		MainWindow(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MainWindow()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::RichTextBox^  richTextBox1;
+	private: System::Windows::Forms::TableLayoutPanel^  tableLayoutPanel1;
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::ListView^  listView1;
+	private: System::Windows::Forms::ColumnHeader^  dateAdded;
+
+	private: System::Windows::Forms::ColumnHeader^  details;
+	private: System::Windows::Forms::ColumnHeader^  status;
+
+
+
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(MainWindow::typeid));
+			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
+			this->tableLayoutPanel1 = (gcnew System::Windows::Forms::TableLayoutPanel());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->listView1 = (gcnew System::Windows::Forms::ListView());
+			this->dateAdded = (gcnew System::Windows::Forms::ColumnHeader());
+			this->details = (gcnew System::Windows::Forms::ColumnHeader());
+			this->status = (gcnew System::Windows::Forms::ColumnHeader());
+			this->tableLayoutPanel1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// richTextBox1
+			// 
+			this->richTextBox1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->richTextBox1->Location = System::Drawing::Point(4, 425);
+			this->richTextBox1->Name = L"richTextBox1";
+			this->richTextBox1->ReadOnly = true;
+			this->richTextBox1->ScrollBars = System::Windows::Forms::RichTextBoxScrollBars::Vertical;
+			this->richTextBox1->Size = System::Drawing::Size(756, 120);
+			this->richTextBox1->TabIndex = 1;
+			this->richTextBox1->TabStop = false;
+			this->richTextBox1->Text = L"Hello! Welcome to our program! Wheeeeeeee~ :)";
+			// 
+			// tableLayoutPanel1
+			// 
+			this->tableLayoutPanel1->CellBorderStyle = System::Windows::Forms::TableLayoutPanelCellBorderStyle::Single;
+			this->tableLayoutPanel1->ColumnCount = 1;
+			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent, 
+				50)));
+			this->tableLayoutPanel1->Controls->Add(this->richTextBox1, 0, 1);
+			this->tableLayoutPanel1->Controls->Add(this->textBox1, 0, 2);
+			this->tableLayoutPanel1->Controls->Add(this->listView1, 0, 0);
+			this->tableLayoutPanel1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->tableLayoutPanel1->Location = System::Drawing::Point(0, 0);
+			this->tableLayoutPanel1->Name = L"tableLayoutPanel1";
+			this->tableLayoutPanel1->RowCount = 3;
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 76.82481F)));
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 23.17518F)));
+			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Absolute, 34)));
+			this->tableLayoutPanel1->Size = System::Drawing::Size(764, 585);
+			this->tableLayoutPanel1->TabIndex = 1;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->textBox1->Location = System::Drawing::Point(4, 552);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(756, 26);
+			this->textBox1->TabIndex = 0;
+			this->textBox1->KeyDown += gcnew System::Windows::Forms::KeyEventHandler(this, &MainWindow::textBox1_KeyDown);
+			// 
+			// listView1
+			// 
+			this->listView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::ColumnHeader^  >(3) {this->dateAdded, this->details, 
+				this->status});
+			this->listView1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->listView1->FullRowSelect = true;
+			this->listView1->GridLines = true;
+			this->listView1->Location = System::Drawing::Point(4, 4);
+			this->listView1->Name = L"listView1";
+			this->listView1->Size = System::Drawing::Size(756, 414);
+			this->listView1->TabIndex = 2;
+			this->listView1->UseCompatibleStateImageBehavior = false;
+			this->listView1->View = System::Windows::Forms::View::Details;
+			// 
+			// dateAdded
+			// 
+			this->dateAdded->Text = L"Date Added";
+			this->dateAdded->Width = 203;
+			// 
+			// details
+			// 
+			this->details->Text = L"Details";
+			this->details->Width = 364;
+			// 
+			// status
+			// 
+			this->status->Text = L"Status";
+			this->status->Width = 151;
+			// 
+			// MainWindow
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(764, 585);
+			this->Controls->Add(this->tableLayoutPanel1);
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^  >(resources->GetObject(L"$this.Icon")));
+			this->MaximizeBox = false;
+			this->Name = L"MainWindow";
+			this->Text = L"MainWindow";
+			this->Load += gcnew System::EventHandler(this, &MainWindow::MainWindow_Load);
+			this->tableLayoutPanel1->ResumeLayout(false);
+			this->tableLayoutPanel1->PerformLayout();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+
+		// Public Methods
+	public: void addToList(String^ details);
+	public: std::string getUserInput(void);
+	public: void clearInputField(void);
+	public: void appendToOutput(std::string feedbackToUser);
+	//public: void addTaskToList(Task task);  // stub function
+
+		// Events
+	private: System::Void textBox1_KeyDown(System::Object^  sender, System::Windows::Forms::KeyEventArgs^  e);
+	private: System::Void MainWindow_Load(System::Object^  sender, System::EventArgs^  e) {
+			 }
+};
+}
+
