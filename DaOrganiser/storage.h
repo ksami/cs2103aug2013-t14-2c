228ed17dd6c5e3f2f6a13800d314e734c12dca41
areky2 <areky2@gmail.com>
2013-09-25 16:18:44 +0000

    new edited

--- DaOrganiser/storage.h
+++ DaOrganiser/storage.h
@@ -4,18 +4,27 @@
 #include <fstream>
 #include <algorithm>
 #include <vector>
-
 using namespace std;
-class storage{
-	private:
-		ofstream outputStream;
-      const char STORAGE_NAME[] = "storage.txt";
-  
-	public:
-		void initialOutputStream();
-		void writeAllToFile(vector<task> allTask);
-		void travelAllVector(vector<task> allTask, void (storage::*work)(task));
-		void writeOneToFile(task t);
-		char* contentOfTask(task t);
-		void closeOutPutStream();
+
+class storage {
+private:
+	ofstream outputStream;
+	ifstream inputStream;
+	static char STORAGE_NAME[99]; 
+
+public:
+	storage();
+	~storage();
+	bool isSafeToOpenStream();
+	void initialOutputStream();
+	void writeAllToFile(vector<task> allTask);
+	void travelAllVector(vector<task> allTask, void (storage::*work)(task));
+	void writeOneToFile(task t);
+	char* getContentOfTask(task t);
+	void closeOutputStream();
+	void initialInputStream();
+	vector<task> readAllFromFile();
+	task readOneFromFile();
+	task putContentIntoTask(char* temp);
+	void closeInputStream();	
 };
