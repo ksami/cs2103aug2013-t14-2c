05a28ffa14b03b524363a09d90ff4c37551730b4
Michael <Michael@localhost>
2013-10-08 15:22:19 +0800

    little modification on storage(incomplete)

--- DaOrganiser/storage.h
+++ DaOrganiser/storage.h
@@ -1,22 +1,37 @@
+#ifndef STORAGE_H
+
 #include <iostream>
 #include <fstream>
 #include <algorithm>
 #include <vector>
+#include "task.h"
 
 using namespace std;
+
+template <class T>
 class storage{
 	private:
 		ofstream outputStream;
+		ifstream inputStream;
+		char* STORAGE_NAME;
+
 	public:
+		storage(char);
+		~storage();
+		bool isSafeToOpenStream();
 		void initialOutputStream();
 		void writeAllToFile(vector<task>);
-		void travelAllVector(vector<task>, void (storage::*work)());
+		void travelAllVector(vector<task>, void (storage::*work)(task));
 		void writeOneToFile(task);
-		char* contentOfTask(task);
-		void closeOutPutStream();
+		char* getContentOfTask(task);
+
+		void closeOutputStream();
 		void initialInputStream();
 		vector<task> readAllFromFile();
 		task readOneFromFile();
 		task putContentIntoTask(char*);
 		void closeInputStream();
+		
 };
+
+#endif // !STORAGE_H
