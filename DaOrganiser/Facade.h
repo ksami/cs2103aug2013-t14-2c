e10b96056e5eb45a0243857deae67aa8efd9b6d7
Nicholas <areky2@gmail.com>
2013-11-05 13:51:54 +0800

    refactor Time and Date

---
+++ DaOrganiser/Facade.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Task.h"
+#include "TaskManager.h"
+#include "Parser.h"
+#include "Storage.h"
+
+using namespace std;
+
+class Facade {
+private:
+		void* guiInterface;
+        string _userCommand;
+		string searchKey;
+		string deleteInput;
+        vector<string> commandInput;
+		vector<Task> taskStorage;
+		vector<Task> keySearch;
+		TaskManager p;
+		Storage* saveFile;
+public:
+        Facade();
+		Facade(void*);
+        ~Facade();
+		void executeProgramme();
+		void executeProgramme(bool& toExit);
+		
+		vector<Task> getTaskStorage();
+
+        bool inputCommand();
+		bool checkCommand();
+        void parserCommand();
+		bool executeCommand();
+		
+		bool checkDeleteInput();
+		bool checkSearchKey();
+        void displayCommand();
+        void displayVector();
+		void setUserCommand(string input); //for testing
+};
