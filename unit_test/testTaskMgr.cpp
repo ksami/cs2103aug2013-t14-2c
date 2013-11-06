4d56a5a1e05b3c841b5975e309c74b4788d2e5eb
Michael <Michael@localhost>
2013-11-07 00:16:34 +0800

    testCase added, enough? or need more

--- unit_test/testTaskMgr.cpp
+++ unit_test/testTaskMgr.cpp
@@ -1,4 +1,4 @@
-/*#include "testTaskMgr.h"
+#include "testTaskMgr.h"
 
 
 TEST_F(testTaskMgr, create_emptystorage_emptystring_returnfalse) {
@@ -59,4 +59,67 @@ TEST_F(testTaskMgr, create_emptystorage_overlapdate_returnfalse) {
 	EXPECT_FALSE(createTaskSuccess);
 }
 
-*/
+TEST_F(testTaskMgr, create_emptystorage_missingParameter_returntrue) {
+	vector<string> testString;
+	testString.push_back("-startdate");
+	testString.push_back("");
+	testString.push_back("-enddate");
+	testString.push_back("");
+	testString.push_back("-starttime");
+	testString.push_back("");
+	testString.push_back("-endtime");
+	testString.push_back("");
+	testString.push_back("-details");
+	testString.push_back("");
+	bool createTaskSuccess = taskMgrUnderTest.createTask(testString, stubTaskStorage);
+	EXPECT_TRUE(createTaskSuccess);
+}
+
+TEST_F(testTaskMgr, create_emptystorage_duplicatedParameter_returnFalse) {
+	vector<string> testString;
+	testString.push_back("-startdate");
+	testString.push_back("-startdate");
+	testString.push_back("-details");
+	testString.push_back("-details");
+	testString.push_back("");
+	bool createTaskSuccess = taskMgrUnderTest.createTask(testString, stubTaskStorage);
+	EXPECT_FALSE(createTaskSuccess);
+}
+
+TEST_F(testTaskMgr, create_emptystorage_invliadStartDate_returnFalse) {
+	vector<string> testString;
+	testString.push_back("-startdate");	
+	testString.push_back("hudjs");
+	bool createTaskSuccess = taskMgrUnderTest.createTask(testString, stubTaskStorage);
+	EXPECT_FALSE(createTaskSuccess);
+}
+
+TEST_F(testTaskMgr, create_emptystorage_invliadStartTime_returnFalse) {
+	vector<string> testString;
+	testString.push_back("-starttime");	
+	testString.push_back("hudjs");
+	bool createTaskSuccess = taskMgrUnderTest.createTask(testString, stubTaskStorage);
+	EXPECT_FALSE(createTaskSuccess);
+}
+
+TEST_F(testTaskMgr, create_emptystorage_invliadEndDate_returnFalse) {
+	vector<string> testString;
+	testString.push_back("-enddate");	
+	testString.push_back("hudjs");
+	bool createTaskSuccess = taskMgrUnderTest.createTask(testString, stubTaskStorage);
+	EXPECT_FALSE(createTaskSuccess);
+}
+
+TEST_F(testTaskMgr, create_emptystorage_invliadEndTime_returnFalse) {
+	vector<string> testString;
+	testString.push_back("-endtime");	
+	testString.push_back("hudjs");
+	bool createTaskSuccess = taskMgrUnderTest.createTask(testString, stubTaskStorage);
+	EXPECT_FALSE(createTaskSuccess);
+}
+
+TEST_F(testTaskMgr, create_emptystorage_noParameter_returnTrue) {
+	vector<string> testString;
+	bool createTaskSuccess = taskMgrUnderTest.createTask(testString, stubTaskStorage);
+	EXPECT_TRUE(createTaskSuccess);
+}
