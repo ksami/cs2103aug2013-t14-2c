f399dbd61cc874ef267d43df5be0c77d902e266d
Nicholas <areky2@gmail.com>
2013-11-07 22:57:04 +0800

    GUI testing

--- DaOrganiser/taskmanager.h
+++ DaOrganiser/taskmanager.h
@@ -30,9 +30,12 @@ public:
 	void undoTask(vector<Task>&);
 	void redoTask(vector<Task>&);
 	
+	bool checkDateTimeLogic(Task &,char);
+	bool checkTime(string);
 	bool checkRecordUndoCommand();
 	bool checkRecordRedoCommand();
 	bool checkID(int,vector<Task>,int&);
+
 	void updateIDNumber();
 	void reupdateIDNumber(vector<Task>&);
 	void reupdateStatus(vector<Task>&);
@@ -40,20 +43,18 @@ public:
 	void insertTaskExecuted(int, string);
 	void storeTask(Task);
 	void testingRecordCommand();
-	bool searchKeyBlank(vector<Task> &,vector<Task> &, string);
 	int findIDPos(int,vector<Task>);
-
+	string extractDetails(int,vector<string>);
+	bool searchKeyBlank(vector<Task> &,vector<Task> &, string);
 	int searchIDToBeUpdated(vector<string>);
 	int stringToInt(string);
 	bool checkDateValue(int);
-	bool assignDate(Task &,int,char);
 	bool getDayMonthYear(int,date &);
-	bool assignTime(Task &,int,char);
 	bool getHourMin(int,time_s &);
-	string extractDetails(int,vector<string>);
+
+	bool assignTime(Task &,int,char);
+	bool assignDate(Task &,int,char);
 	void assignSystemTime(Task &,char);
 	void assignSystemDate(Task &,char);
-	bool checkDateTimeLogic(Task &,char);
-	bool checkTime(string);
 };
 
